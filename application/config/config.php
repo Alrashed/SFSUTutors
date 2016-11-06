6ff51aadae500a510fe8b0ba85b5660a6b07d1ff
Khalid Alrashed <Khalid@sfs-wifi-dhcp-10-143-70-24.wifi.sfsu.edu>
2016-11-05 19:23:46 -0700

    Update mysql credentials for mini

--- application/config/config.php
+++ application/config/config.php
@@ -54,7 +54,7 @@ define('URL', URL_PROTOCOL . URL_DOMAIN . URL_SUB_FOLDER);
  */
 define('DB_TYPE', 'mysql');
 define('DB_HOST', '127.0.0.1');
-define('DB_NAME', 'mini');
-define('DB_USER', 'root');
-define('DB_PASS', 'your_password');
+define('DB_NAME', 'sfsututors');
+define('DB_USER', 'khalid');
+define('DB_PASS', 'mysql1234');
 define('DB_CHARSET', 'utf8');
