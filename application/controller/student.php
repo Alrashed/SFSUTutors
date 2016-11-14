d9695df50cb7eb6d9e4c0e66bf6cdc1fad622c7b
Khalid Alrashed <Khalid@Khalids-MBP.T-mobile.com>
2016-11-13 17:42:26 -0800

    Add student and tutor registration controller functionality

--- application/controller/student.php
+++ application/controller/student.php
@@ -2,5 +2,20 @@
 
 class Student extends Controller
 {
+    public function register()
+    {
+        require APP . 'view/_templates/header.php';
+        require APP . 'view/student/register.php';
+        require APP . 'view/_templates/footer.php';
+    }
     
+    public function registerStudent()
+    {
+        if (isset($_POST["submit"])) {
+
+            $this->model->createStudent($_POST["firstname"], $_POST["lastname"], $_POST["email"], $_POST["password"], $_POST["birthdate"], $_POST["phone"]);
+        }
+        
+        header('location: ' . URL . 'login/index');
+    }
 }
--- application/controller/tutor.php
+++ application/controller/tutor.php
@@ -2,5 +2,20 @@
 
 class Tutor extends Controller
 {
+    public function register()
+    {
+        require APP . 'view/_templates/header.php';
+        require APP . 'view/tutor/register.php';
+        require APP . 'view/_templates/footer.php';
+    }
     
+    public function registerTutor()
+    {
+        if (isset($_POST["submit"])) {
+
+            $this->model->createTutor($_POST["firstname"], $_POST["lastname"], $_POST["email"], $_POST["password"], $_POST["birthdate"], $_POST["phone"], $_POST["major"], $_POST["gpa"], $_POST["about"], $_POST["available"], $_POST["offering"], $_POST["price"]);
+        }
+        
+        header('location: ' . URL . 'login/index');
+    }
 }
